06a6a55c91e4779b73425fa184cd19a5944aa69d
SAEED1799 <73116797+SAEED1799@users.noreply.github.com>
2021-03-24 17:38:05 +0200

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -1,37 +1,140 @@
-#include <iostream>
 #include "Board.hpp"
+#include <iostream>
+#include <stdexcept>
 
 using namespace std;
-namespace ariel {
-    void Board::post (uint row, uint column, Direction direction, string message){
-        if(message==""){
-        __throw_invalid_argument("not inpup");
-        }
-        else{
-            if(Direction::Horizontal==direction){
-                for(uint i =0 ;i<message.length();i++){
-                     board[row][column+i]=message.at(i);
-                }
-            }
-            else{
-                 for(uint i =0 ;i<message.length();i++){
-                     board[row+i][column]=message.at(i);
-            }
-        }
-    }
-    }
-    string Board::read(uint row, uint column, Direction direction, uint length){
-        return " ";
-    }
-    void Board::show (){
-        for(uint i=0 ; i<board.size();i++){
-            cout<<to_string(i)+":\t";
-            for(uint j =0 ; j<board[i].size();j++){
-                cout<<board[i][j];
 
-            }
-            cout<<endl;
-        }
+namespace ariel {
+
+char Board::empty_val = '_';
+
+typedef unsigned int uint;
+typedef unsigned long ulong;
+/**
+ * this method is a setter for a givem position, 
+ * Its not a reference due to the fact that i dont wish accidently creating an empty space,
+ * hence i must put a character in each and every place i create.
+ * */
+void Board::setCharAt(uint x, uint y, char c) {
+    board[{x, y}] = c;
+}
+
+/**
+ * this method returns the actuall character on x,y but if its empty it will return "_";
+ * */
+char Board::charAt(uint x, uint y) const {
+    auto key = make_pair(x, y);
+    if (board.count(key) == 1) {
+        return board.at(key);
     }
+    return empty_val;
 
 }
+
+/**
+* given the start and end position of the newest message calculate the new bound of the "show" square.
+ * */
+void Board::updateBound(ulong start_x, ulong start_y, ulong size_x, ulong size_y) {
+    //cout << start_x << " : " << start_y << " : " << size_x << " : " << size_y << endl;
+
+    // if the new message starts after the min value then we need to offset the 'real' length of the message.
+    // end  = new_start - old_start + length.
+    if (start_x > bound.min_x) {
+        size_x += start_x - bound.min_x;
+    }
+
+    // same but for the y axis.
+    if (start_y > bound.min_y) {
+        size_y += start_y - bound.min_y;
+    }
+
+    if (bound.min_x > start_x) {
+        bound.min_x = start_x;
+    }
+
+    if (bound.min_y > start_y) {
+        bound.min_y = start_y;
+    }
+
+    if (bound.size_x < size_x) {
+        bound.size_x = size_x;
+    }
+
+    if (bound.size_y < size_y) {
+        bound.size_y = size_y;
+    }
+}
+
+void Board::post(uint row, uint column, Direction direction, const std::string &message) {
+    uint length = message.length();
+    if (length == 0) {
+        // do nothing
+        return;
+    }
+    // if (length == 0) {
+    //     throw invalid_argument{"Cannot post message of length : 0"};
+    // }
+
+    if (direction == Direction::Horizontal) {
+        updateBound(column, row, length, 1);
+        for (uint i = 0; i < length; i++) {
+            uint x = column + i;
+            //charAt(x, row) = message.at(i);
+            setCharAt(x, row, message.at(i));
+        }
+    } else {
+        updateBound(column, row, 1, length);
+        for (uint i = 0; i < length; i++) {
+            uint y = row + i;
+            setCharAt(column, y, message.at(i));
+            //charAt(column, y) = message.at(i);
+        }
+    }
+}
+
+std::string Board::read(uint row, uint column, Direction direction, uint length) const {
+    if (length == 0) {
+        return "";
+    }
+    // if (length == 0) {
+    //     throw invalid_argument{"Cannot read message of length : 0"};
+    // }
+
+    string message;
+    if (direction == Direction::Horizontal) {
+        for (uint i = 0; i < length; i++) {
+            uint x = column + i;
+            message += charAt(x, row);
+        }
+    } else {
+        for (uint i = 0; i < length; i++) {
+            uint y = row + i;
+            message += charAt(column, y);
+        }
+    }
+
+    return message;
+}
+
+void Board::show() const {
+    if (bound.size_x == 0) {
+        return;
+    }
+
+    //cout << "Bound {" << bound.min_x << " ," << bound.min_y << " :: " << bound.size_x << " ," << bound.size_y << " }" << endl;
+
+    uint pre_length = to_string(bound.min_y + bound.size_y + 2).length() + 2;
+    uint length = bound.size_x + 4;
+    for (uint i = 0; i < bound.size_y + 2; i++) {
+        //cout << length << endl;
+        uint y = bound.min_y + i - 1;
+
+        auto space = to_string(y) + ": ";
+        while (space.length() < pre_length) {
+            space += " ";
+        }
+        cout << space << read(y, bound.min_x - 2, Direction::Horizontal, length) << endl;
+    }
+}
+
+} // namespace ariel
--- Board.hpp
+++ Board.hpp
@@ -1,20 +1,62 @@
-#pragma once
+#ifndef BOARD_CPP
+#define BOARD_CPP
+
 #include "Direction.hpp"
+#include <map>
 #include <string>
-#include <vector>
-namespace ariel{
-    class Board{
-      unsigned int ROW;
-      unsigned int CAL;
-     std::vector<std::vector<char>> board;
-      public:
-      Board(){
-     board = std::vector<std::vector<char>>(35,std::vector<char>(35,'_'));
-      };
 
-       void post(uint  row, uint column, Direction direction, std::string message);
-        static std::string read(uint row, uint column, Direction direction,  uint length);
-        void show();
-    };
+struct Bound {
+    // public:
+    unsigned long min_x;
+    unsigned long size_x;
+    unsigned long min_y;
+    unsigned long size_y;
+    Bound() {
+        size_x = 0;
+        size_y = 0;
+        min_x = (unsigned long)(-1);
+        min_y = (unsigned long)(-1);
+    }
+};
 
-}
+namespace ariel {
+class Board {
+private:
+    static char empty_val;
+    Bound bound;
+    std::map<std::pair<unsigned int, unsigned int>, char>
+        board;
+    void setCharAt(unsigned int x, unsigned int y, char c);
+    char charAt(unsigned int x, unsigned int y) const;
+
+    void updateBound(unsigned long start_x, unsigned long start_y, unsigned long size_x, unsigned long size_y);
+
+public:
+    /**
+     * Post a message on the board, note that old message on the same spot will get overwriten.
+     * row, columnm - positon
+     * direction - the direction of the message horizontal - left to right,  Vertical - Top to Button.
+     * string - the actuall message
+     * */
+    void post(unsigned int row, unsigned int column, Direction direction, const std::string &message);
+    /**
+     * Read the message from the board starting from given possition and going in the specified direction,
+     * stop when read "length" worth of letters.
+     * row, columnm - positon
+     * direction - the direction of the message horizontal - left to right,  Vertical - Top to Button.
+     * length - the length of the post to read ( a.k.a the length of the returned string ).
+     * */
+    std::string read(unsigned int row, unsigned int column, Direction direction, unsigned int length) const;
+
+    /**
+     * print on screen, the board,
+     * min index shown would be for X : Max(0, bound().minX()) - 1), Y : Max(0,  bound().miny()
+     * max index shown would befor X : bound().maxX() + 1, Y : bound().maxY() + 1
+     * 
+     * print everything in that area.
+     * */
+    void show() const;
+};
+} // namespace ariel
+
+#endif
